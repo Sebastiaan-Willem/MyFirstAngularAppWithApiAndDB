2cebfd5a2193d5d4bc98287298ba135db2028780
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-05-12 10:54:19 +0200

    update display member frontend

--- Angular/appusers/src/app/services/member.service.ts
+++ Angular/appusers/src/app/services/member.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Member } from '../Members'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
-  baseUrl='https://localhost:44389/api/members';
+  baseUrl='https://localhost:44389/api/users/members';
+
+  //add token to HTTP Header via HttpOptions
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem('user') || '{}').token}`
+    })
+  };
 
   constructor(private httpClient: HttpClient) { }
 
   getMembers(){
-    return this.httpClient.get<Member[]>(this.baseUrl);
+    return this.httpClient.get<Member[]>(this.baseUrl, this.httpOptions);
   }
 
   getMember(id: number){
-    return this.httpClient.get<Member>(this.baseUrl);
+    return this.httpClient.get<Member>(`this.baseUrl/${id}`, this.httpOptions);
   }
 }
